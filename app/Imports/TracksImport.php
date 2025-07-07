41efe3c6221f92165dd239d8980fc4405fd7772b
FateevMaxim <fateev.maxim@gmail.com>
2025-07-07 15:35:46 +0500

    new china

--- app/Imports/TracksImport.php
+++ app/Imports/TracksImport.php
@@ -28,9 +28,9 @@ class TracksImport implements ToModel, SkipsOnError, WithEvents
     */
     public function model(array $row)
     {
-        if (isset($row[0])){
+        if (isset($row[1])){
             return new TrackList([
-                'track_code' => $row[0],
+                'track_code' => $row[1],
                 'to_china' => $this->date,
                 'status' => 'Получено в Китае',
                 'reg_china' => 1,
